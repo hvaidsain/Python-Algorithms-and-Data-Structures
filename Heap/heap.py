311818c7a8dbb8f76badf9be5710e20c2c7644ea
Harsh Vaidsain <harsh.vaidsain@gmail.com>
2020-04-14 14:28:11 +0530

    added insert and getmax funtons in heap module

--- Heap/heap.py
+++ Heap/heap.py
@@ -1,5 +1,6 @@
-
+import sys
 # takes an array and index q of the element that we need to heapify
+# time complexity : O(logn)
 def heapify(a,i):
     n = len(a)
     left = 2*i +1
@@ -19,7 +20,7 @@ def heapify(a,i):
         a[largest] = temp
         heapify(a,largest)
 
-
+# time complexity : O(n)
 def buildHeap(a):
 
     n = len(a)
@@ -29,6 +30,7 @@ def buildHeap(a):
     for i in range(l,-1,-1):
         heapify(a,i)
 
+# time complexity : O(logn)
 def getMax(a):
 
     if len(a)==0:
@@ -40,6 +42,35 @@ def getMax(a):
     heapify(a,0)
 
     return maxi
+
+
+
+# time complexity : O(logn)
+def increaseKey(a,i,key):
+    if(i>=len(a)):
+        return "key index to large"
+    if(len(a)==0):
+        return "Heap is empty"
+
+    a[i] = key
+    while(i>0 and a[i//2] < a[i]):
+        temp = a[i//2]
+        a[i//2] = a[i]
+        a[i] = temp
+        
+        i = i//2
+
+def insert(a,key):
+
+    a.append(-9)
+
+    i = len(a) - 1
+
+    increaseKey(a,i,key)
+
+
+
+
         
 
 
--- Heap/main.py
+++ Heap/main.py
@@ -1,6 +1,6 @@
 import heap
 
-a = [1,2,2,3,4,5,6,7,8,9,10]
+a = [1,2,3,4,5,6,7,8,9,10]
 
 print(f"before building heap {a}")
 
@@ -12,10 +12,23 @@ print(f"max : {heap.getMax(a)}")
 
 print(f"latest array status {a}")
 
-print(f"max : {heap.getMax(a)}")
+heap.increaseKey(a,3,400)
 
 print(f"latest array status {a}")
 
-print(f"max : {heap.getMax(a)}")
+heap.increaseKey(a,8,30)
 
 print(f"latest array status {a}")
+
+heap.insert(a,15)
+print(f"latest array status {a}")
+
+heap.insert(a,12)
+print(f"latest array status {a}")
+
+heap.insert(a,500)
+print(f"latest array status {a}")
+
+heap.insert(a,50)
+print(f"latest array status {a}")
+
